7ad795457476a0c7e2a6df7089acb78b4ec699c5
Lafon Christophe <lafonchristophe5@gmail.com>
2024-03-03 23:34:31 +0100

    game: new sync system

--- src/Components/audioNBeat.js
+++ src/Components/audioNBeat.js
@@ -18,10 +18,38 @@ const AudioNBeat = ({
     }
   };
 
-  //---- MUSIC and BEATS INITIALIZERS ----
+  //---- MUSIC INITIALIZER ----
+  // useEffect(() => {
+  //   if (playing) {
+  //     audioRef.current.play();
+  //   } else {
+  //     audioRef.current.pause();
+  //     setCurrentTime(false);
+  //     audioRef.current.currentTime = 0;
+  //     clearInterval(intervalId);
+  //   }
+
+  //   // eslint-disable-next-line
+  // }, [playing]);
+
   useEffect(() => {
+    //---- v2 ----
     if (playing) {
-      audioRef.current.play();
+      let id = null;
+      let tempo = 121;
+      setTimeout(() => {
+        audioRef.current.play();
+        let beatIntervalId = setInterval(() => {
+          setBeat((prevBeat) => prevBeat + 1);
+        }, tempo);
+
+        id = beatIntervalId;
+        setIntervalId(id);
+
+        return () => {
+          clearInterval(beatIntervalId);
+        };
+      }, 100);
     } else {
       audioRef.current.pause();
       setCurrentTime(false);
@@ -33,26 +61,39 @@ const AudioNBeat = ({
   }, [playing]);
 
   useEffect(() => {
+    // ---- v2 ----
     if (currentTime) {
-      let id = null;
-      let tempo = 121;
       setTimeout(() => {
-        let beatIntervalId = setInterval(() => {
-          setBeat((prevBeat) => prevBeat + 1);
-        }, tempo);
-
-        id = beatIntervalId;
-        setIntervalId(id);
-
-        return () => {
-          clearInterval(beatIntervalId);
-        };
+        setBeat(20);
       }, 2000);
       // }, 880);
     }
 
     // eslint-disable-next-line
   }, [currentTime]);
+
+  // ---- BEATS INITIALIZER ----
+  // useEffect(() => {
+  //   if (currentTime) {
+  //     let id = null;
+  //     let tempo = 121;
+  //     setTimeout(() => {
+  //       let beatIntervalId = setInterval(() => {
+  //         setBeat((prevBeat) => prevBeat + 1);
+  //       }, tempo);
+
+  //       id = beatIntervalId;
+  //       setIntervalId(id);
+
+  //       return () => {
+  //         clearInterval(beatIntervalId);
+  //       };
+  //     }, 2000);
+  //     // }, 880);
+  //   }
+
+  //   // eslint-disable-next-line
+  // }, [currentTime]);
 
   return (
     <audio
