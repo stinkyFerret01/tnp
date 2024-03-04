61e17564f81f6784ec6d2292ed86e24ca8c7222b
Lafon Christophe <lafonchristophe5@gmail.com>
2024-03-04 22:13:35 +0100

    tesst new sync model

--- src/Components/audioContext.js
+++ src/Components/audioContext.js
@@ -1,75 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const AudioContext = ({ audioCommand, setBeat, isPlaying, setIsPlaying }) => {
-  const [musicRef, setMusicRef] = useState(null);
+  // const [musicRef, setMusicRef] = useState(null);
+  // const audioRef = "../Audio/HBFSp04.mp3";
+  const audioRef = useRef("../Audio/HBFSp04.mp3");
 
   const [startAudioDate, setStartAudioDate] = useState(0);
   const [startAudioDatePlus, setStartAudioDatePlus] = useState(0);
 
   const [checkTimeIntervalId, setCheckTimeIntervalId] = useState(null);
 
+  // useEffect(() => {
+  //   clearInterval(checkTimeIntervalId);
+  //   // console.log(audioCommand);
+  //   if (audioCommand.actionX === "play") {
+  //     const audioContext = new (window.AudioContext ||
+  //       window.webkitAudioContext)();
+
+  //     const loadAudio = async () => {
+  //       const response = await fetch("../Audio/HBFSp04.mp3");
+  //       const audioData = await response.arrayBuffer();
+  //       const audioBuffer = await audioContext.decodeAudioData(audioData);
+
+  //       const source = audioContext.createBufferSource();
+  //       source.buffer = audioBuffer;
+
+  //       source.connect(audioContext.destination);
+  //       console.log("1", audioContext.state);
+
+  //       //---- EXECUTE start music ----
+  //       source.start(0);
+  //       let executeAudioStart = Date.now();
+  //       setStartAudioDate(Date.now());
+
+  //       setMusicRef(source);
+
+  //       //---- TRIGGER isPlaying ----
+  //       const checkTime = () => {
+  //         let time = Date.now();
+  //         let dif = time - 1900 - executeAudioStart;
+
+  //         if (dif >= -2 && dif <= 2) {
+  //           source.start(0, 20);
+  //           setStartAudioDatePlus(Date.now());
+  //           console.log(dif);
+  //           clearInterval(checkTimeIntervalId);
+  //           setIsPlaying(true);
+  //           setTimeout(() => {
+  //             setBeat(20);
+  //           }, 320);
+
+  //           return () => {
+  //             audioContext.close();
+  //           };
+  //         }
+  //       };
+
+  //       const interval = setInterval(() => {
+  //         checkTime();
+  //       }, 1);
+  //       setCheckTimeIntervalId(interval);
+  //     };
+
+  //     loadAudio();
+
+  //     return () => {
+  //       audioContext.close();
+  //     };
+  //   } else if (musicRef) {
+  //     musicRef.stop();
+  //     setIsPlaying(false);
+  //   }
+
+  //   // eslint-disable-next-line
+  // }, [audioCommand]);
+
   useEffect(() => {
-    clearInterval(checkTimeIntervalId);
-    // console.log(audioCommand);
+    console.log(audioCommand);
     if (audioCommand.actionX === "play") {
-      const audioContext = new (window.AudioContext ||
-        window.webkitAudioContext)();
-
-      const loadAudio = async () => {
-        const response = await fetch("../Audio/HBFSp04.mp3");
-        const audioData = await response.arrayBuffer();
-        const audioBuffer = await audioContext.decodeAudioData(audioData);
-
-        const source = audioContext.createBufferSource();
-        source.buffer = audioBuffer;
-
-        source.connect(audioContext.destination);
-        console.log("1", audioContext.state);
-
-        //---- EXECUTE start music ----
-        source.start(0);
-        let executeAudioStart = Date.now();
-        setStartAudioDate(Date.now());
-
-        setMusicRef(source);
-
-        //---- TRIGGER isPlaying ----
-        const checkTime = () => {
-          let time = Date.now();
-          let dif = time - 1900 - executeAudioStart;
-
-          if (dif >= -2 && dif <= 2) {
-            setStartAudioDatePlus(Date.now());
-            console.log(dif);
-            clearInterval(checkTimeIntervalId);
-            setIsPlaying(true);
-            setTimeout(() => {
-              setBeat(20);
-            }, 320);
-
-            return () => {
-              audioContext.close();
-            };
-          }
-        };
-
-        const interval = setInterval(() => {
-          checkTime();
-        }, 1);
-        setCheckTimeIntervalId(interval);
-      };
-
-      loadAudio();
-
-      return () => {
-        audioContext.close();
-      };
-    } else if (musicRef) {
-      musicRef.stop();
-      setIsPlaying(false);
+      audioRef.current.play();
+      setTimeout(() => {
+        audioRef.current.currentTime = 20;
+      }, 2000);
+    } else if (audioCommand.actionX === "stop") {
+      audioRef.current.pause();
     }
-
-    // eslint-disable-next-line
   }, [audioCommand]);
 
   // -- TOCHECK --
@@ -84,6 +99,12 @@ const AudioContext = ({ audioCommand, setBeat, isPlaying, setIsPlaying }) => {
       {/* <div style={{ backgroundColor: "red" }}>
         {<p>settingBeatDate --: {settingBeatDate}</p>}
       </div> */}
+      <audio
+        ref={audioRef}
+        controls
+        type="audio/mp3"
+        src="../Audio/HBFSp04.mp3"
+      ></audio>
     </div>
   );
 };
