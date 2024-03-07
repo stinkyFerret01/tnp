7c80e4dc2dc8900802324ba56a565b83cf62d6d4
Lafon Christophe <lafonchristophe5@gmail.com>
2024-03-07 01:30:47 +0100

    fix: no double play allowed

--- src/Components/homePage.js
+++ src/Components/homePage.js
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = ({ setAudioCommand, isPlaying }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
     if (isPlaying) {
       navigate("/game");
     }
@@ -12,17 +16,25 @@ const HomePage = ({ setAudioCommand, isPlaying }) => {
 
   //--> starts the game and navigates to gamePage when isPlaying trigged
   const handlePlayButtonClick = () => {
-    setAudioCommand({
-      actionX: "play",
-      timex: 0,
-      beatx: 0,
-    });
+    setIsLoading(true);
   };
 
-  return (
+  useEffect(() => {
+    if (isLoading) {
+      setAudioCommand({
+        actionX: "play",
+        timex: 0,
+        beatx: 0,
+      });
+    }
+  }, [isLoading, setAudioCommand]);
+
+  return !isLoading ? (
     <div>
       <button onClick={handlePlayButtonClick}>PLAY</button>
     </div>
+  ) : (
+    <div>LOADING</div>
   );
 };
 
